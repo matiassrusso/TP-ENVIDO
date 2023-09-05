52ad14344241f0c4acdfcab11fc789cc71bb890b
ruxxit0 <73359875+matiassrusso@users.noreply.github.com>
2023-09-05 20:54:02 -0300

    listo envido nashe
    ahora solo falta el juego papu

--- tpenvido2.py
+++ tpenvido2.py
@@ -26,59 +26,58 @@ def carta(array_carta):                  #CREADOR CARTAS [numero, palo]
     array_carta[1].extend([num_carta(), palo_carta()])
     array_carta[2].extend([num_carta(), palo_carta()])
 
-carta(cartasJ1)
-carta(cartasJ2)
 
 
-totenvido=[0,0]
-envidoJ1=totenvido[0]
-envidoJ2=totenvido[1]
+
+
+pre_envidoJ1=0
+pre_envidoJ2=0
 
 
 def envido(array_carta, array_envido):
-    if array_carta[0][1] in array_carta[0] == array_carta[1][1] in array_carta[1]:      #SI EL PALO DE LA PRIMERA CARTA ES IGUAL AL PALO DE LA SEGUNDA
+    if array_carta[0][1]== array_carta[1][1]:      #SI EL PALO DE LA PRIMERA CARTA ES IGUAL AL PALO DE LA SEGUNDA
         array_envido=20
-        if array_carta[0][0] in array_carta[0] not in (10, 11, 12) and array_carta[1][0] in array_carta[1] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
-            array_envido+=array_carta[0][0] in array_carta[0]+array_carta[1][0]in array_carta[1]
-        elif array_carta[0][0] in array_carta[0] in (10, 11, 12) and array_carta[1][0] in array_carta[1] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
+        if array_carta[0][0] not in (10, 11, 12) and array_carta[1][0] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
+            array_envido+=array_carta[0][0] +array_carta[1][0]
+        elif array_carta[0][0] in (10, 11, 12) and array_carta[1][0] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
             array_envido+=0
-        elif array_carta[0][0] in array_carta[0] not in (10, 11, 12) and array_carta[1][0] in array_carta[1] in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
+        elif array_carta[0][0] not in (10, 11, 12) and array_carta[1][0]in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
             array_envido+=array_carta[0][0]
-        elif array_carta[0][0] in array_carta[0] in (10, 11, 12) and array_carta[1][0] in array_carta[1] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
+        elif array_carta[0][0]in (10, 11, 12) and array_carta[1][0] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
             array_envido+=array_carta[1][0]
 
 
-    elif array_carta[0][1] in array_carta[0] == array_carta[2][1] in array_carta[2]:    #SI EL PALO DE LA PRIMERA CARTA ES IGUAL AL PALO DE LA TERCERA
+    elif array_carta[0][1] == array_carta[2][1]:    #SI EL PALO DE LA PRIMERA CARTA ES IGUAL AL PALO DE LA TERCERA
         array_envido=20
-        if array_carta[0][0] in array_carta[0] not in (10, 11, 12) and array_carta[2][0] in array_carta[2] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
-            array_envido+=array_carta[0][0] in array_carta[0]+array_carta[2][0]in array_carta[2]
+        if array_carta[0][0] not in (10, 11, 12) and array_carta[2][0] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
+            array_envido+=array_carta[0][0] + array_carta[2][0]
 
-        elif array_carta[0][0] in array_carta[0] in (10, 11, 12) and array_carta[2][0] in array_carta[2] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
+        elif array_carta[0][0] in (10, 11, 12) and array_carta[2][0] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
             array_envido+=0
 
-        elif array_carta[0][0] in array_carta[0] not in (10, 11, 12) and array_carta[2][0] in array_carta[2] in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
+        elif array_carta[0][0] not in (10, 11, 12) and array_carta[2][0] in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
             array_envido+=array_carta[0][0]
 
-        elif array_carta[0][0] in array_carta[0] in (10, 11, 12) and array_carta[2][0] in array_carta[2] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
+        elif array_carta[0][0] in (10, 11, 12) and array_carta[2][0] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
             array_envido+=array_carta[2][0]
         
 
-    elif array_carta[2][1] in array_carta[2] == array_carta[1][1] in array_carta[1]:    #SI EL PALO DE LA TERCERA ES IGUAL AL PALO DE LA SEGUNDA
+    elif array_carta[2][1] == array_carta[1][1]:    #SI EL PALO DE LA TERCERA ES IGUAL AL PALO DE LA SEGUNDA
         array_envido=20
-        if array_carta[2][0] in array_carta[2] not in (10, 11, 12) and array_carta[1][0] in array_carta[1] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
-            array_envido+=array_carta[2][0] in array_carta[2]+array_carta[1][0]in array_carta[1]
+        if array_carta[2][0] not in (10, 11, 12) and array_carta[1][0] not in (10, 11, 12):     #SI LOS NUMEROS DE LAS 2 CARTAS NO SON 10-11-12, SE SUMAN
+            array_envido+=array_carta[2][0] + array_carta[1][0]
 
-        elif array_carta[2][0] in array_carta[2] in (10, 11, 12) and array_carta[1][0] in array_carta[1] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
+        elif array_carta[2][0] in (10, 11, 12) and array_carta[1][0] in (10, 11, 12):           #SI LOS NUMEROS DE LAS 2 CARTAS SON 10-11-12, NO SUMA NADA
             array_envido+=0
 
-        elif array_carta[2][0] in array_carta[2] not in (10, 11, 12) and array_carta[1][0] in array_carta[1] in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
+        elif array_carta[2][0] not in (10, 11, 12) and array_carta[1][0] in (10, 11, 12):       #SI LOS NUMEROS DE LA PRIMERA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA PRIMERA
             array_envido+=array_carta[2][0]
 
-        elif array_carta[2][0] in array_carta[2] in (10, 11, 12) and array_carta[1][0] in array_carta[1] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
+        elif array_carta[2][0] in (10, 11, 12) and array_carta[1][0] not in (10, 11, 12):       #SI LOS NUMEROS DE LA SEGUNDA CARTA NO SON 10-11-12, PERO DE LA SEGUNDA SI, SE SUMA SOLO LA SEGUNDA
             array_envido+=array_carta[1][0]
 
 
-    elif array_carta[2][1] in array_carta[2] == array_carta[1][1] in array_carta[1] == array_carta[0][1] in array_carta[0]:     #SITUACION FLOR
+    elif array_carta[2][1] == array_carta[1][1] == array_carta[0][1]:     #SITUACION FLOR
         array_envido=20
         if array_carta[2][0] not in (10,11,12) and array_carta[1][0] not in (10,11,12) and array_carta[0][0] not in (10,11,12):
             templist=[]
@@ -94,22 +93,43 @@ def envido(array_carta, array_envido):
 
         elif array_carta[2][0] not in (10,11,12) and array_carta[1][0] not in (10,11,12) and array_carta[0][0] in (10,11,12):
             array_envido+=array_carta[1][0]+array_carta[2][0]
+    
+    elif array_carta[2][1] != array_carta[1][1] != array_carta[0][1]:     #SITUACION TODOS DISTINTOS
+        templist=[]
+        templist.extend([array_carta[2][0], array_carta[1][0], array_carta[0][0]])
+        templist.sort(reverse=True)
+        if templist[0] not in (10,11,12):
+            array_envido+=templist[0]
+        elif templist[1] not in (10, 11, 12):
+            array_envido+=templist[1]
+        elif templist[2] not in (10, 11,12):
+            array_envido+=templist[2]
+        else:
+            array_envido=0
+
+        
+
+    array_envido = int(array_envido)
+    return array_envido
 
 
-
-num_carta()
-palo_carta()
+carta(cartasJ1)
+carta(cartasJ2)
 print(cartasJ1)
 print(cartasJ2)
 print(cartasJ1[0][1])
-print(f'Carta usuario: {cartasJ1[0][0]} de {cartasJ1[0][1]}')
-templist=[]
-x=0
-templist.extend([cartasJ1[2][0], cartasJ1[1][0], cartasJ1[0][0]])
-templist.sort(reverse=True)
-print(templist)
-x=templist[0]+templist[1]
-print(x)
+print(cartasJ1[1][1])
+
+print(f'Carta 1 de usuario: {cartasJ1[0][0]} de {cartasJ1[0][1]}')
+
+
+
+envidoJ1= envido(cartasJ1,pre_envidoJ1)
+envidoJ2 = envido(cartasJ2,pre_envidoJ2)
+print("Valor del envido J1:", envidoJ1)
+print("Valor del envido J2:", envidoJ2)
+
+
 
 
 
